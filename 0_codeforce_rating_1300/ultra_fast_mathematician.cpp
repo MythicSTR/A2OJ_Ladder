6eb0478617e5750e4b1d8435f510a464daad6044
Shishir Tamrakar <stamrakar545@gmail.com>
2021-12-03 22:31:22 +0545

    Solved Ultra Fast Mathematician

---
+++ 0_codeforce_rating_1300/ultra_fast_mathematician.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    string a, b, c;
+    cin >> a >> b;
+ 
+    for(int i =0; i<a.length(); i++) {
+        if(a[i] == b[i]) {
+            c += "0";
+        } else {
+            c += "1";
+        }
+    }
+ 
+    cout << c;
+ 
+    return 0;
+}
