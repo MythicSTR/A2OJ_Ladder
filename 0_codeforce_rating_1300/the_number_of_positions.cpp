2f6e25936f620b736090e58ba0dcacdb3b9000e9
Shishir Tamrakar <stamrakar545@gmail.com>
2022-01-03 21:47:57 +0545

    Solved The Number Of Positions

---
+++ 0_codeforce_rating_1300/the_number_of_positions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, a, b, count(0);
+    cin >> n >> a >> b;
+
+    for(int i = 1; i <= n; ++i) {
+        if((i-1) >= a && (n - i) <= b) {
+            ++count;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
